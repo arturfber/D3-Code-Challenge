ece151ec017c3204caf114aa3d0bb86afcb0b885
arturfber <artur.fb.95@gmail.com>
2021-03-18 00:11:31 -0300

    Terminado ligação do DOM ao código

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
+document.querySelector('#submit').addEventListener('click', function(e){
+    let valor = Number(document.querySelector('#valor').value)
+    predict(valor)
+})
 
-function predict(v1,v2){
-    return parseInt(v1) + parseInt(v2)
+
+function predict(dias){
+
+    // Rejeitar valores menores que 0
+    if(dias > 0){
+
+        // Número total de casos confirmados no Brasil até o dia 17/03/2021
+        let casosConfirmados = 11706639
+        // Taxa de transmissão estipulada no Brasil até o dia 17/03/2021
+        let taxaTransmissao = 1.23
+        console.log("Total de dias: " + dias)
+                   
+        let result = document.querySelector('#resultado')
+
+        for(count = 1; count <= dias; count+=1){
+
+            result.innerHTML = result.innerHTML + '<li>' + count + ' -> ' + casosConfirmados + '</li>'
+            casosConfirmados = casosConfirmados * taxaTransmissao
+
+        }
+    }        
+    else{
+        console.log("Por favor insira um valor maior que 0.")
+    }
 }
-
-alert(predict(prompt(),prompt()))
 
