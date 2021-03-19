eacd87cd9df4d387bddda5d161f25ab240cab6f2
arturfber <artur.fb.95@gmail.com>
2021-03-18 23:39:33 -0300

    Ponto e vírgula e função para apagar valor do input ao ser clicado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 document.querySelector('#submit').addEventListener('click', function(e){
-    let valor = Number(document.querySelector('#valor').value)
-    predict(valor)
+    let valor = Number(document.querySelector('#valor').value);
+    predict(valor);
 })
+
+document.querySelector("#valor").addEventListener('click', function(e){
+    apagarInput()
+});
 
 
 function predict(dias){
@@ -10,21 +14,26 @@ function predict(dias){
     if(dias > 0){
 
         // Número total de casos confirmados no Brasil até o dia 17/03/2021
-        let casosConfirmados = 11706639
+        let casosConfirmados = 11706639;
         // Taxa de transmissão estipulada no Brasil até o dia 17/03/2021
-        let taxaTransmissao = 1.23
-        console.log("Total de dias: " + dias)
+        let taxaTransmissao = 1.23;
+        console.log("Total de dias: " + dias);
                    
-        let result = document.querySelector('#resultado')
+        let result = document.querySelector('#resultado');
 
         for(count = 1; count <= dias; count+=1){
 
-            result.innerHTML = result.innerHTML + '<li>' + count + ' 🡆 ' + casosConfirmados.toLocaleString('en-US') + '</li>'
-            casosConfirmados = casosConfirmados * taxaTransmissao
-
+            result.innerHTML = result.innerHTML + '<li>' + count + ' 🡆 ' + casosConfirmados.toLocaleString('en-US') + '</li>';
+            casosConfirmados = casosConfirmados * taxaTransmissao;
+            
         }
     }        
     else{
-        console.log("Por favor insira um valor maior que 0.")
+        document.querySelector('#resultado').innerHTML = "Por favor, insira um valor maior que 0."
+        console.log("Por favor, insira um valor maior que 0.");
     }
+}
+
+function apagarInput(){
+    document.querySelector('#valor').value = ''
 }
